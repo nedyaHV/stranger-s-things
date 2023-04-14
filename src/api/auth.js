7ec5a16710c728578d1112ebafe525e4fde98115
Hayden Vick <vickhayden@gmail.com>
2023-04-14 10:28:16 -0500

    adding register/login/getMe

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,9 +1,33 @@
 import React from "react";
+import { RouterProvider } from "react-router-dom";
 
 const COHORT = "2303-FTB-ET-WEB-FT"
 const API = `https://strangers-things.herokuapp.com/api/${COHORT}`
 
-const loginUser = async (userObject) => {
+export const getMe = async (token) => {
+  try {
+    const response = await fetch(
+      `${API}/users/me`, {
+        method: "GET",
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      })
+      const { success, error, data} = await response.json();
+      if(success) {
+        return data;
+      } 
+      if(!success && error) {
+        return error;
+      }
+      return;
+  } catch (error) {
+    console.error(error)
+  }
+};
+
+export const loginUser = async (userObject) => {
     try {
         const response = await fetch(
             `${API}/users/login`, {
@@ -29,6 +53,33 @@ const loginUser = async (userObject) => {
     catch(error) {
         console.error(error);
     }
-}
+};
 
-export default loginUser;
+export const registerUser = async (userObject) => {
+  try {
+      const response = await fetch(
+          `${API}/users/register`, {
+          method: "POST",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(userObject)
+        });
+      // const result = response.json();
+      const { success, error, data} = await response.json();
+      console.log({success, error, data});
+      if (success) {
+        const { token, message } = data;
+        localStorage.setItem("token", token);
+        return { token, message};
+      }
+      if (!success && !error) {
+        const { name, message } = data;
+        return { name, message};
+      }
+      console.log(success, error, data);
+  }
+  catch(error) {
+      console.error(error);
+  }
+};
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,4 @@ export {default as Navbar} from './Navbar';
 export {default as Login} from './Login';
 export {default as Posts} from './Posts';
 export {default as Welcome} from './Welcome';
-export {default as LoginRegister} from './LoginRegister';
-export {default as loginUser} from '../api/auth';
+export {default as Register} from './Register';
